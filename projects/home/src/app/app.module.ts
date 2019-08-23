966e76449bf868591f9941ad58a06c2f2a255af3
Atticus Pomerantz <atticus.pomerantz@dealerteamwork.com>
2019-08-23 12:00:58 -0500

    part 2 complete

---
+++ projects/home/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { Ng7CommonModule } from '@ng7/common';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    Ng7CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
